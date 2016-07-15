b63ac0e41e3d1f7e99e368e9ddf8b4b982e6b418
Karthik <karthik@krishnaswamy17@gmail.com>
2016-07-15 10:18:25 +0530

    Initial commit- Updating BucketDrops src Project.

---
+++ app/src/main/java/com/ks17/bucketdrops/ActivityMain.java
@@ -0,0 +1,13 @@
+package com.ks17.bucketdrops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
