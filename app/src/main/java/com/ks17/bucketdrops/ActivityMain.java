42d0ecf2ca58f119aa31e544f052166fa2494d4b
Karthik <karthik@krishnaswamy17@gmail.com>
2016-08-09 21:52:54 +0530

    Added background and Button functionality.

--- app/src/main/java/com/ks17/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/ks17/bucketdrops/ActivityMain.java
@@ -3,13 +3,17 @@ package com.ks17.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
     Toolbar toolbar;
     ImageView imageView;
+    Button bt_click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,14 @@ public class ActivityMain extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setBackground();
+        bt_click = (Button) findViewById(R.id.btn_drop);
+        bt_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this, "Clicked from Anonymous class", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void setBackground() {
