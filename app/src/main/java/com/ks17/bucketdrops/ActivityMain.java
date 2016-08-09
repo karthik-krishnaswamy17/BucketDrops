076572acd0a5604b4685e88bb8979ff38284d842
Karthik <karthik@krishnaswamy17@gmail.com>
2016-08-09 16:59:28 +0530

    Added Image Resource Library Glide and mapped background.

--- app/src/main/java/com/ks17/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/ks17/bucketdrops/ActivityMain.java
@@ -1,13 +1,28 @@
 package com.ks17.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setBackground();
+    }
+
+    private void setBackground() {
+        imageView = (ImageView) findViewById(R.id.bd_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().
+                into(imageView);
+
     }
 }
